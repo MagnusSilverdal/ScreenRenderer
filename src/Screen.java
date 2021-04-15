823ca09eaa1abc56d0615c037d376bd8624a4f55
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2021-04-15 13:29:03 +0200

    Implemented second part of the fill. Works for clockwise-oriented triangles with second point as top. Some errors generation of first an last line for corner-cases. Triangles with flat top misses first line. Triangles with first corner above last gets mis-rendered at the last line. Lots of debug-code in this commit...

--- src/Screen.java
+++ src/Screen.java
@@ -42,13 +42,20 @@ public class Screen {
     }
 
     public void fillTriangle(Triangle t, int color) {
+        System.out.println("--" + color);
         Line[] triangle = t.getEdges();
         // Only works for triangles with 2:nd Point as top, need to order them
         int[] pixelsLine1 = getPointsFromLine(triangle[0].getEnd(),triangle[0].getStart());
         int[] pixelsLine2 = getPointsFromLine(triangle[1].getStart(),triangle[1].getEnd());
-        int[] pixelsLine3 = getPointsFromLine(triangle[2].getEnd(),triangle[2].getStart());
+        int[] pixelsLine3;
+        if (triangle[2].getEnd().getY() < triangle[2].getStart().getY()) {
+            pixelsLine3 = getPointsFromLine(triangle[2].getEnd(),triangle[2].getStart());
+        } else {
+            pixelsLine3 = getPointsFromLine(triangle[2].getStart(),triangle[2].getEnd());
+        }
+
         // Debug...
-        for (int i = 0 ; i < pixelsLine1.length ; i+=2) {
+        /*for (int i = 0 ; i < pixelsLine1.length ; i+=2) {
             System.out.println("("+pixelsLine1[i]+","+pixelsLine1[i+1]+")");
         }
         System.out.println();
@@ -59,24 +66,87 @@ public class Screen {
         for (int i = 0 ; i < pixelsLine3.length ; i+=2) {
             System.out.println("("+pixelsLine3[i]+","+pixelsLine3[i+1]+")");
         }
-        System.out.println();
+        System.out.println();*/
         // end Debug..
 
         int index1 = 0;
         int index2 = 0;
         while (index1 <= pixelsLine1.length-1 && index2 <= pixelsLine2.length-1) {
-            while(index1 < pixelsLine1.length-3 && pixelsLine1[index1+1] == pixelsLine1[index1+3]) {
-                index1+=2;
-            }
             while(index2 < pixelsLine2.length-3 && pixelsLine2[index2+1] == pixelsLine2[index2+3]) {
                 index2+=2;
             }
+            System.out.println("u ("+pixelsLine1[index1]+","+pixelsLine1[index1+1]+") - ("+pixelsLine2[index2]+","+pixelsLine2[index2+1]+")");
             for (int x = pixelsLine1[index1] ; x <= pixelsLine2[index2] ; x++) {
                 drawPixel(x,pixelsLine1[index1+1],color);
+            }
+            while(index1 < pixelsLine1.length-3 && pixelsLine1[index1+1] == pixelsLine1[index1+3]) {
+                index1+=2;
             }
             index1+=2;
             index2+=2;
         }
+
+        int index3 = 0;
+        //while (index3 < pixelsLine3.length - 3 && pixelsLine3[index3 + 1] == pixelsLine1[index1 - 1]) {
+        //    index3 += 2;
+        //}
+            if (triangle[2].getStart().getY() > triangle[2].getEnd().getY()) {
+                System.out.println(index3 + " " + index2);
+
+                while (index3 < pixelsLine3.length - 1 && index2 < pixelsLine2.length - 1 && pixelsLine3[index3 + 1] < pixelsLine2[index2 + 1]) {
+                    index3 += 2;
+                }
+                while (index3 <= pixelsLine3.length - 1 && index2 <= pixelsLine2.length - 1) {
+                    while (index2 < pixelsLine2.length - 3 && pixelsLine2[index2 + 1] == pixelsLine2[index2 + 3]) {
+                        index2 += 2;
+                    }
+                    System.out.println("l\\ ("+pixelsLine3[index3]+","+pixelsLine3[index3+1]+") - ("+pixelsLine2[index2]+","+pixelsLine2[index2+1]+")");
+                    for (int x = pixelsLine3[index3]; x <= pixelsLine2[index2]; x++) {
+                        drawPixel(x, pixelsLine3[index3 + 1], color);
+                    }
+                    while (index3 <= pixelsLine3.length - 3 && pixelsLine3[index3 + 1] == pixelsLine3[index3 + 3]) {
+                        index3 += 2;
+                    }
+                    index3 += 2;
+                    index2 += 2;
+                }
+                /*System.out.println("l\\ ("+pixelsLine3[index3]+","+pixelsLine3[index3+1]+") - ("+pixelsLine2[index2]+
+                        ","+pixelsLine2[index2+1]+")");
+                for (int x = pixelsLine3[index3]; x <= pixelsLine2[index2]; x++) {
+                    drawPixel(x, pixelsLine3[index3 + 1], color);
+                }*/
+                System.out.println("Halt");
+            }else {
+                System.out.println(index1 + " " + index3 );
+                while (index3 < pixelsLine3.length - 1 && index1 < pixelsLine1.length - 1 && pixelsLine3[index3 + 1] < pixelsLine1[index1 + 1]) {
+                        index3 += 2;
+                    }
+                    while (index3 <= pixelsLine3.length-1 && index1 <= pixelsLine1.length-1) {
+                        while (index3 < pixelsLine3.length - 3 && pixelsLine3[index3 + 1] == pixelsLine3[index3 + 3]) {
+                            index3 += 2;
+                        }
+
+                        System.out.println("l/ ("+pixelsLine1[index1]+","+pixelsLine1[index1+1]+") - ("+pixelsLine3[index3]+","+pixelsLine3[index3+1]+")");
+                        for (int x = pixelsLine1[index1]; x <= pixelsLine3[index3]; x++) {
+                            drawPixel(x, pixelsLine3[index3 + 1], color);
+                        }
+                        while (index1 < pixelsLine1.length - 3 && pixelsLine1[index1 + 1] == pixelsLine1[index1 + 3]) {
+                            index1 += 2;
+                        }
+                        index3 += 2;
+                        index1 += 2;
+                    }
+            }
+
+        /*for (Line l : triangle) {
+            int[] pixels = getPointsFromLine(l.getStart(),l.getEnd());
+            System.out.println();
+            for (int i = 0 ; i < pixels.length ; i++) {
+                System.out.println("("+pixels[i]+","+pixels[i+1]+")");
+                drawPixel(pixels[i++],pixels[i],color);
+            }
+        }*/
+
     }
 
     public int[] getPointsFromLine(Point start, Point end) {
--- src/ScreenRendererTest.java
+++ src/ScreenRendererTest.java
@@ -15,19 +15,29 @@ public class ScreenRendererTest extends ScreenRenderer{
     private Triangle t3 = new Triangle(new Point(150,50),new Point(170,20),new Point(190,60));
     private Triangle t4 = new Triangle(new Point(210,50),new Point(230,50),new Point(250,60));
 
+    private Triangle t1t = new Triangle(new Point(30,90),new Point(50,60),new Point(70,90));
+    private Triangle t2t = new Triangle(new Point(90,90),new Point(110,60),new Point(130,80));
+    private Triangle t3t = new Triangle(new Point(150,90),new Point(170,60),new Point(190,100));
+    private Triangle t4t = new Triangle(new Point(210,90),new Point(230,90),new Point(250,100));
+
+
     public ScreenRendererTest() {
         super(width*scale, height*scale, scale);
     }
 
     public void update() {
+        getScreen().drawTriangle(t1t,0xffffff);
+        getScreen().drawTriangle(t2t,0xffffff);
+        getScreen().drawTriangle(t3t,0xffffff);
+        getScreen().drawTriangle(t4t,0xffffff);
         getScreen().fillTriangle(t1,0x00ffff);
         getScreen().fillTriangle(t2,0xff00ff);
         getScreen().fillTriangle(t3,0xffff00);
         getScreen().fillTriangle(t4,0x00ff00);
-        getScreen().drawTriangle(t1,0xffffff);
+/*        getScreen().drawTriangle(t1,0xffffff);
         getScreen().drawTriangle(t2,0xffffff);
         getScreen().drawTriangle(t3,0xffffff);
-        getScreen().drawTriangle(t4,0xffffff);
+        getScreen().drawTriangle(t4,0xffffff);*/
     }
 
     public static void main(String[] args) {
